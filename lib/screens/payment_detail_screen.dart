d96eea9ca59ad4ecbde84e8bcc1522a9928963f0
Rilo Supriyatno <rilobahtiar21@gmail.com>
2025-06-14 11:15:25 +0700

    Refactor payment service imports: rename paymentStorageService.dart to payment_storage_service.dart and update import statements across relevant files

--- lib/screens/payment_detail_screen.dart
+++ lib/screens/payment_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import '../services/paymentStorageService.dart';
+import '../services/payment_storage_service.dart';
 import '../widgets/utils/classic_app_bar.dart';
 import 'package:intl/intl.dart';
 
